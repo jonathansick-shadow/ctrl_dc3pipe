91cc965070ba9633994b1b27cec2dfe41dbe2a9f
ktlim <ktlim@git.lsstcorp.org>
2009-04-14 23:18:07 +0000

    Need to make sure we only use metadata from this visit.

--- python/lsst/ctrl/dc3pipe/CcdMetadataStage.py
+++ python/lsst/ctrl/dc3pipe/CcdMetadataStage.py
@@ -4,6 +4,8 @@ from lsst.daf.persistence import LogicalLocation, DbStorage
 class CcdMetadataStage(Stage):
     def preprocess(self):
         self.activeClipboard = self.inputQueue.getNextDataset()
+        fpaExposureId0 = self.activeClipboard.get('visit0').get('exposureId')
+        fpaExposureId1 = self.activeClipboard.get('visit1').get('exposureId')
 
         db = DbStorage()
         loc = LogicalLocation("%(dbUrl)")
@@ -13,18 +15,21 @@ class CcdMetadataStage(Stage):
             INSERT INTO Raw_CCD_Exposure
             SELECT DISTINCT rawCCDExposureId, rawFPAExposureId
             FROM Raw_Amp_Exposure
-        """)
+            WHERE rawFPAExposureId = %d OR rawFPAExposureId = %d
+        """ % (fpaExposureId0, fpaExposureId1) )
         db.executeSql("""
             INSERT INTO Science_CCD_Exposure
             SELECT DISTINCT
                 scienceCCDExposureId, scienceFPAExposureId, rawCCDExposureId
             FROM Science_Amp_Exposure
-        """)
+            WHERE scienceFPAExposureId = %d OR scienceFPAExposureId = %d
+        """ % (fpaExposureId0, fpaExposureId1) )
         db.executeSql("""
             INSERT INTO Science_FPA_Exposure
             SELECT DISTINCT scienceFPAExposureId
             FROM Science_Amp_Exposure
-        """)
+            WHERE scienceFPAExposureId = %d OR scienceFPAExposureId = %d
+        """ % (fpaExposureId0, fpaExposureId1) )
         db.endTransaction()
 
         # rely on default postprocess() to move self.activeClipboard to output queue
